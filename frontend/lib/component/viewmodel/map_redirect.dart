f7ab448f6898a0675caf57b3eb84d0455bc38ec7
Pra_Kas <prakash63690@gmail.com>
2024-04-21 10:04:08 +0530

    COORECTED LATTITUDE AND LONGITUDE

--- frontend/lib/component/viewmodel/map_redirect.dart
+++ frontend/lib/component/viewmodel/map_redirect.dart
@@ -2,7 +2,7 @@ import 'package:url_launcher/url_launcher.dart';
 
 class MapRedirect{
   static Future<void>openMap(double lat,double lon)async {
-    String url = "https://www.google.com/maps/search/?api=1&query=$lat,$lon";
+    String url = "https://www.google.com/maps/search/?api=1&query=$lon,$lat";
     // ignore: deprecated_member_use
     if(await(canLaunch(url))){
       // ignore: deprecated_member_use
